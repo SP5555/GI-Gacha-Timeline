8815c3ac0dceb85645b7cd9cdc0c44866be5d86c
Set Paing <setpaing20022002@gmail.com>
2022-07-07 21:57:22 -0700

    Kazuha-banner-update

---
+++ data.js
@@ -0,0 +1,148 @@
+var data = [{
+	"version": "1.0",
+	"banner": [{
+		"5": ["Venti"],
+		"4": ["Barbara", "Fischl", "Xiangling"]
+	},{
+		"5": ["Klee"],
+		"4": ["Noelle", "Sucrose", "Xingqiu"]
+	}]
+},{	
+	"version": "1.1",
+	"banner": [{
+		"5": ["Tartaglia"],
+		"4": ["Beidou", "Diona", "Ningguang"]
+	},{
+		"5": ["Zhongli"],
+		"4": ["Chongyun", "Razor", "Xinyan"]
+	}]
+},{	
+	"version": "1.2",
+	"banner": [{
+		"5": ["Albedo"],
+		"4": ["Bennett", "Fischl", "Sucrose"]
+	},{
+		"5": ["Ganyu"],
+		"4": ["Noelle", "Xiangling", "Xingqiu"]
+	}]
+},{
+	"version": "1.3",
+	"banner": [{
+		"5": ["Xiao"],
+		"4": ["Beidou", "Diona", "Xinyan"]
+	},{
+		"5": ["Keqing"],
+		"4": ["Barbara", "Bennett", "Ningguang"]
+	},{
+		"5": ["Hu Tao"],
+		"4": ["Chongyun", "Xiangling", "Xingqiu"]
+	}]
+},{
+	"version": "1.4",
+	"banner": [{
+		"5": ["Venti"],
+		"4": ["Noelle", "Razor", "Sucrose"]
+	},{
+		"5": ["Tartaglia"],
+		"4": ["Barbara", "Fischl", "Rosaria"]
+	}]
+},{
+	"version": "1.5",
+	"banner": [{
+		"5": ["Zhongli"],
+		"4": ["Diona", "Noelle", "Yanfei"]
+	},{
+		"5": ["Eula"],
+		"4": ["Beidou", "Xingqiu", "Xinyan"]
+	}]
+},{
+	"version": "1.6",
+	"banner": [{
+		"5": ["Klee"],
+		"4": ["Barbara", "Fischl", "Sucrose"]
+	},{
+		"5": ["Kazuha"],
+		"4": ["Bennett", "Razor", "Rosaria"]
+	}]
+},{
+	"version": "2.0",
+	"banner": [{
+		"5": ["Ayaka"],
+		"4": ["Chongyun", "Ningguang", "Yanfei"]
+	},{
+		"5": ["Yoimiya"],
+		"4": ["Diona", "Sayu", "Xinyan"]
+	}]
+},{
+	"version": "2.1",
+	"banner": [{
+		"5": ["Shogun"],
+		"4": ["Sara", "Sucrose", "Xiangling"]
+	},{
+		"5": ["Kokomi"],
+		"4": ["Beidou", "Rosaria", "Xingqiu"]
+	}]
+},{
+	"version": "2.2",
+	"banner": [{
+		"5": ["Tartaglia"],
+		"4": ["Chongyun", "Ningguang", "Yanfei"]
+	},{
+		"5": ["Hu Tao"],
+		"4": ["Diona", "Sayu", "Thoma"]
+	}]
+},{
+	"version": "2.3",
+	"banner": [{
+		"5": ["Albedo", "Eula"],
+		"4": ["Bennett", "Noelle", "Rosaria"]
+	},{
+		"5": ["Itto"],
+		"4": ["Barbara", "Gorou", "Xiangling"]
+	}]
+},{
+	"version": "2.4",
+	"banner": [{
+		"5": ["Shenhe", "Xiao"],
+		"4": ["Chongyun", "Ningguang", "Yun Jin"]
+	},{
+		"5": ["Ganyu", "Zhongli"],
+		"4": ["Beidou", "Xingqiu", "Yanfei"]
+	}]
+},{
+	"version": "2.5",
+	"banner": [{
+		"5": ["Yae"],
+		"4": ["Diona", "Fischl", "Thoma"]
+	},{
+		"5": ["Kokomi", "Shogun"],
+		"4": ["Bennett", "Sara", "Xinyan"]
+	}]
+},{
+	"version": "2.6",
+	"banner": [{
+		"5": ["Ayato", "Venti"],
+		"4": ["Sucrose", "Xiangling", "Yun Jin"]
+	},{
+		"5": ["Ayaka"],
+		"4": ["Razor", "Rosaria", "Sayu"]
+	}]
+},{
+	"version": "2.7",
+	"banner": [{
+		"5": ["Xiao", "Yelan"],
+		"4": ["Barbara", "Noelle", "Yanfei"]
+	},{
+		"5": ["Itto"],
+		"4": ["Chongyun", "Gorou", "Shinobu"]
+	}]
+},{
+	"version": "2.8",
+	"banner": [{
+		"5": ["Kazuha", "Klee"],
+		"4": ["Heizou", "Ningguang", "Thoma"]
+	},{
+		"5": ["Yoimiya"],
+		"4": []
+	}]
+}];
---
+++ script.js
@@ -0,0 +1,163 @@
+var t1 = new Date().getTime();
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function mainLoader(data, img_src) {
+	
+	let main5 = document.getElementById("mainTable5");
+	let main4 = document.getElementById("mainTable4");
+	
+	let main_thead5 = document.getElementById("thead5");
+	let main_thead4 = document.getElementById("thead4");
+	let version_tr5 = document.getElementById("versionRow5");
+	let version_tr4 = document.getElementById("versionRow4");
+
+	let main_tbody5 = document.createElement("tbody");
+	let main_tbody4 = document.createElement("tbody");
+	main5.appendChild(main_tbody5); // main5 tbody append
+	main4.appendChild(main_tbody4); // main4 tbody append
+
+	cha_list = [];
+
+	for (var s = 5; s >= 4; s--) {
+
+		for (var i = 0; i < data.length; i++) {
+			idvData = data[i];
+
+
+			let version_r_td = document.createElement("td");
+			version_r_td.colSpan = idvData["banner"].length;
+			version_r_td.innerHTML = idvData["version"];
+			if (s == 5) {
+				version_tr5.appendChild(version_r_td);
+			} else {
+				version_tr4.appendChild(version_r_td);
+			}
+
+			for (var i1 = 0; i1 < idvData["banner"].length; i1++) {
+				
+				for (var i2 = 0; i2 < idvData["banner"][i1][s].length; i2++) {
+					
+					var character = idvData["banner"][i1][s][i2];
+
+					if (!cha_list.includes(character)) {
+						cha_list.push(character);
+						let cha_tr = document.createElement("tr");
+						let cha_head_td = document.createElement("td");
+						cha_head_td.className = "first-col";
+
+						let img = document.createElement("img");
+						img.className = "char-face";
+						img.src = `https://imgur.com/${img_src[character]}.png`;
+
+						let name_div = document.createElement("div");
+						name_div.className = "char-name";
+						name_div.innerHTML = character;
+
+						let fn_contianer = document.createElement("div");
+						fn_contianer.className = "face-name-container";
+
+						fn_contianer.appendChild(img);
+						fn_contianer.appendChild(name_div);
+						cha_head_td.appendChild(fn_contianer);
+						cha_tr.appendChild(cha_head_td);
+
+						for (var v = 0; v < data.length; v++) {
+							for (var v1 = 0; v1 < data[v]["banner"].length; v1++) {
+								let cha_r_td = document.createElement("td");
+								cha_r_td.className = "tds";
+								cha_r_td.id = `${character}_v${data[v]["version"]}_b${v1}`;
+								let span_text = document.createElement("span")
+								span_text.innerHTML = `${data[v]["version"]}`
+								cha_r_td.appendChild(span_text);
+								cha_tr.appendChild(cha_r_td);
+							}
+						}
+
+						if (s == 5) {
+							main_tbody5.appendChild(cha_tr);
+						} else {
+							main_tbody4.appendChild(cha_tr);
+						}
+					}
+				}
+			}
+		}
+	}
+	let space5 = document.createElement("div"); space5.className = "vertical-space";
+	let space4 = document.createElement("div"); space4.className = "vertical-space";
+	main_tbody5.appendChild(space5);
+	main_tbody4.appendChild(space4);
+}
+
+function visualizer(data) {
+	var charDict = {};
+
+	for (var i = 0; i < data.length; i++) {
+		idvData = data[i];
+
+		for (var i1 = 0; i1 < idvData["banner"].length; i1++) {
+			// 5 star colorize
+			for (var i2 = 0; i2 < idvData["banner"][i1]["5"].length; i2++) {
+				character = idvData["banner"][i1]["5"][i2];
+
+				if (charDict[character] == undefined) {
+					charDict[character] = 1;
+				} else {
+					charDict[character]++;
+				}
+
+				let tdbox = document.getElementById(`${character}_v${idvData["version"]}_b${i1}`)
+				for (var x = 12; x > 0; x--) {
+					try {
+						c1 = x.toString(16);
+						c2 = (x-1).toString(16);
+						tdbox.style = `background: linear-gradient(to right, #${c1}${c1}${c1}, #${c2}${c2}${c2});`;
+
+						if (x == 12) {
+							let banner_note = document.createElement("div");
+							banner_note.className = "banner-note";
+							let char_txt = document.createElement("span"); char_txt.className="no-wrap";
+							char_txt.innerHTML = character;
+							let v_txt = document.createElement("span"); v_txt.className="no-wrap";
+							if (charDict[character] == 1) {
+								v_txt.innerHTML = `Release`;
+							} else {
+								v_txt.innerHTML = `Rerun ${charDict[character] - 1}`;
+							}
+							banner_note.appendChild(char_txt);
+							banner_note.innerHTML += '\n';
+							banner_note.appendChild(v_txt);
+							tdbox.appendChild(banner_note);
+						}
+						
+						tdbox = tdbox.nextSibling;
+					}
+					catch(e) {}
+				}
+			}
+			// 4 star colorize
+			for (var i2 = 0; i2 < idvData["banner"][i1]["4"].length; i2++) {
+				character = idvData["banner"][i1]["4"][i2];
+				let tdbox = document.getElementById(`${character}_v${idvData["version"]}_b${i1}`)
+				for (var x = 12; x > 0; x-=3) {
+					try {
+						c1 = x.toString(16);
+						c2 = (x-3).toString(16);
+						tdbox.style = `background: linear-gradient(to right, #${c1}${c1}${c1}, #${c2}${c2}${c2});`;
+						tdbox = tdbox.nextSibling;
+					}
+					catch(e) {}
+				}
+			}
+		}
+	}
+}
+
+mainLoader(data, img_src);
+visualizer(data);
+
+var t2 = new Date().getTime();
+document.getElementById("loadTime").innerHTML = `Loading Time - ${t2 - t1}ms`
